27e8b1fc63f2793da31434a71feb5c47ef0a758a
Jetro Tull Roa <jetrotullroa@gmail.com>
2016-07-12 17:44:08 -0700

    setup reducer and create initial data for BookList

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'JS: The Bad Parts' },
+    { title: 'Harry Pooper' },
+    { title: 'Hungry James' },
+    { title: 'Shadowhater' },
+    { title: 'Alex in Neverland' },
+  ]
+}
